33fc3be161d733707e9381c81c87ec93ed3ff37c
katapi <katapi2000@gmail.com>
2018-09-23 01:19:28 +0900

    ポート番号変更

--- server.py
+++ server.py
@@ -183,5 +183,5 @@ if __name__ == "__main__":
 
     #app.run(host='0.0.0.0', port=80, debug=True)
     # どこからでもアクセスできるよ
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=8000)
 
