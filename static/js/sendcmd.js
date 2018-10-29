23b8b4fb21ea07444489c426753afaf6f17de130
Massu <imomushi.pipi@gmail.com>
2018-10-29 17:38:34 +0900

    ajaxで非同期通信するように

---
+++ static/js/sendcmd.js
@@ -0,0 +1,11 @@
+$(".btn").on('click',function() {
+    // ajaxを用いて非同期通信
+    $.ajax({
+        url: $(this).parent().attr("id"),   // 送信URL
+        type: 'post',
+        data: $(this).attr("name"),
+        dataType: 'text'
+
+    });
+    
+});
